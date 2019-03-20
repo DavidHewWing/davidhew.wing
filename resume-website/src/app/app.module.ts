c9824d5f7416d00904da5d3b64c6a3758113d112
David Hew-wing <dhew67@gmail.com>
2019-03-19 23:42:37 -0400

    color scheme and pictures*

--- resume-website/src/app/app.module.ts
+++ resume-website/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { MaterialModule } from 'src/presentation/material-module';
 import { AppRoutingModule } from '../routing/app-routing.module';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +12,7 @@ import { AppRoutingModule } from '../routing/app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
