10eaee1bf7a80183b748fe09e8dd40ca22007ccd
David Hew-wing <dhew67@gmail.com>
2019-03-21 20:25:41 -0400

    refractoring and profile banner picture

--- resume-website/src/app/home-page/home-page.module.ts
+++ resume-website/src/app/home-page/home-page.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { SlideShowComponent } from './slide-show/slide-show.component';
 import { HomePageComponent } from './home-page.component';
+import { MaterialModule } from 'src/presentation/material-module';
 
 @NgModule({
   declarations: [SlideShowComponent, HomePageComponent],
   imports: [
+    MaterialModule,
     CommonModule,
     HomePageRoutingModule
   ]
