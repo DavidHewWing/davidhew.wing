d973dc622af32b490fe2efd1778a6927d4d24da9
David Hew-wing <dhew67@gmail.com>
2019-03-26 18:07:08 -0400

    fixed word formatting on home-page

--- resume-website/src/app/app.component.ts
+++ resume-website/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, ViewChild, HostListener, ElementRef } from '@angular/core';
+import { Component, ViewChild, HostListener, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'resume-website';
   divWidth = 0;
@@ -17,5 +17,10 @@ export class AppComponent {
     }
   }
 
+  ngOnInit() {
+    if (this.parentDiv) {
+      this.divWidth = this.parentDiv.nativeElement.clientWidth;
+    }
+  }
 
 }
