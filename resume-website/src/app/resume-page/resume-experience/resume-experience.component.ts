e29f7371cfd3b957ae606f5bda2badfa19ce78be
David Hew-wing <dhew67@gmail.com>
2019-05-15 15:06:19 -0400

    typos and updated resume

--- resume-website/src/app/resume-page/resume-experience/resume-experience.component.ts
+++ resume-website/src/app/resume-page/resume-experience/resume-experience.component.ts
@@ -20,8 +20,8 @@ export class ResumeExperienceComponent implements OnInit {
     const uottawaCard = new Card('University of Ottawa',
      'Second-Year Software Engineering:  2017-Current',
      './assets/img/uottawa.jpg',
-     'Enrolled at uOttawa Software Engineering in 2017, and currently entering my fourth study term. My phenomenal professors are ' +
-     'pushing me to be more innovative and become a better problem solver. My experience in the uOttawa GeeGees Ultimate Frisbee Team ' +
+     'Enrolled at uOttawa Software Engineering in 2017, and currently undergoing fourth study term. My phenomenal professors are ' +
+     'pushing me to be more innovative and become a better problem solver. My experience on the uOttawa GeeGees Ultimate Frisbee Team ' +
      'inspires me to be a great leader and teaches me to always strive to be better. Most importantly uOttawa helped me to discover' +
      ' my passion for Software Engineering.');
     const cfiaCard = new Card('Canadian Food Insepction Agency',
@@ -38,8 +38,8 @@ export class ResumeExperienceComponent implements OnInit {
     'Software Developer Intern: Jan 2019 - May 2019. Kanata, Ontario',
     './assets/img/jsi-building.jpg',
     'JSI Telecom was a great gateway into the software engineering professionalism world. I practiced the basic concepts of AGILE' +
-    ' which is much more in depth and though out then I once thought. JSI was a customer-driven company, I experienced' +
-    ' a culture in which was welcoming to new customers and dependable to old customers, showing me that empathy and understanding' +
+    ' which is much more in depth and fleshed out then I once thought. JSI is a customer-driven company, I experienced' +
+    ' a culture which was welcoming to new customers and dependable to old customers, showing me that empathy and understanding' +
     ' goes a long way. The one thing that I am most inspired from at JSI was my co-workers. At JSI anyone was willing to take time off' +
     ' their day to share their knowledge. My co-workers allowed my confidence to develop quality software to grow.' +
     ' \n\n In the future, I will try to emulate the characteristics, and principles that my co-workers showed.'
